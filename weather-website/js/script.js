22039895f1ffd2566ad656d2762f59e1c182a8f0
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-11-06 13:48:31 -0300

    06/11

---
+++ weather-website/js/script.js
@@ -0,0 +1,19 @@
+let latitude = -24.503541;
+let longitude = -47.848328;
+
+const apiBaseUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min&timezone=auto`;
+
+
+
+async function getForecast() {
+    
+
+    
+}
+
+try {
+
+
+} catch (e) {
+
+}
