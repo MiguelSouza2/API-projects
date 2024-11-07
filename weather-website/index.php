3beb1d445fa37ce703b42403b6ee95ed03367ae0
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-11-07 15:27:00 -0300

    07/11

--- weather-website/index.php
+++ weather-website/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clima Agora</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <header class="header">
         <h1 class="site-title">Clima Agora</h1>
@@ -35,21 +37,20 @@
 
         <section class="forecast">
             <div class="forecast-item">
-                <span id="day-detail"></span>
-                <span id="temperature-detail"></span>
-                <span id="condition-detail"></span>
+                <span id="day-detail">day-detail</span>
+                <span id="temperature-max-detail">temperature-max-detail</span>
             </div>
             <div class="forecast-item">
-                <span id="day-detail"></span>
-                <span id="temperature-detail"></span>
-                <span id="condition-detail"></span>
+                <span id="day-detail">day-detail</span>
+                <span id="temperature-min-detail">temperature-min-detail</span>
             </div>
 
-            <button onclick="getForecast()">sub</button>
-            
+            <button onclick="getForecast()">submit</button>
+
         </section>
     </main>
 
     <script src="js/script.js"></script>
-    
+</body>
+
 </html>
--- weather-website/js/script.js
+++ weather-website/js/script.js
@@ -1,19 +1,26 @@
 let latitude = -24.503541;
 let longitude = -47.848328;
+let currentDate = new Date();
+let currentHour = currentDate.getHours();
+let currentMinutes = currentDate.getMinutes();
 
 const apiBaseUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min&timezone=auto`;
 
 
 
+
 async function getForecast() {
-    
+    const response = await fetch(apiBaseUrl);
+    const obj = await response.json();
 
-    
+    const day = obj.daily.time[0];
+    const temperatureMax = obj.daily.temperature_2m_max[0];
+    const temperatureMin = obj.daily.temperature_2m_min[0];
+
+
+    document.getElementById("day-detail").innerHTML = day;
+    document.getElementById("temperature-max-detail").innerHTML = temperatureMax;
+    document.getElementById("temperature-min-detail").innerHTML = temperatureMin;
+
 }
 
-try {
-
-
-} catch (e) {
-
-}
