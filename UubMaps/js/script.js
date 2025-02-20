cf860d44d2054ddfbfdad4406ea02f7c9311d518
miguel_casa <130250696+MiguelSouza2@users.noreply.github.com>
2025-02-20 18:17:58 -0300

    j

--- UubMaps/js/script.js
+++ UubMaps/js/script.js
@@ -1,14 +1,35 @@
+
+
+// default coordinates
 var map = L.map("map", {
   center: [-23.5475, -46.63611],
   zoom: 13,
+  inertia: true
 });
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
 
 L.marker([-23.55052, -46.633308])
   .addTo(map)
   .bindPopup("Essa é a cidade de São Paulo")
   .openPopup();
+
+  // add OpenStreetMap tiles to the map
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+
+// set the new coordinates when the user enters new values in the input fields
+function setCoordinates(){
+    let longitude = document.getElementById("longitudeInput").value;
+    let latitude = document.getElementById("latitudeInput").value;
+
+    longitude = parseFloat(longitude);
+    latitude = parseFloat(latitude);
+
+    L.marker().closePopup();
+    
+    var coordinates = [latitude, longitude];
+    L.marker(coordinates).addTo(map);
+    map.panTo(coordinates);
+}
