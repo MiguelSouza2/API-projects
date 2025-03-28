4be233b711e87edc888c42372f9c6ff0b55f53d8
miguel escola <jubscrebis@gmail.com>
2025-03-28 14:27:20 -0300

    28/03

--- UubMaps/js/script.js
+++ UubMaps/js/script.js
@@ -1,17 +1,9 @@
-
-
 // default coordinates
 var map = L.map("map", {
   center: [-23.5475, -46.63611],
   zoom: 13,
   inertia: true
 });
-
-L.marker([-23.55052, -46.633308])
-  .addTo(map)
-  .bindPopup("Essa é a cidade de São Paulo")
-  .openPopup();
-
   // add OpenStreetMap tiles to the map
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
